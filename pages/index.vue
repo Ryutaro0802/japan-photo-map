79551174a2f0c513175d2e195a48709c65124f01
Ryutaro0802 <ne200152g@gmail.com>
2019-07-23 18:55:18 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h1 class="header">Japan Photo Map</h1>
+    <JapanMap prefectureColor="#ffffff"  />
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import { Person } from "~/types"
+import JapanMap from '~/components/JapanMap.vue'
+
+@Component({
+  components: {
+    JapanMap
+  }
+})
+export default class extends Vue {
+  @State people!: Person;
+
+  async created() {
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  color: white;
+}
+
+</style>
