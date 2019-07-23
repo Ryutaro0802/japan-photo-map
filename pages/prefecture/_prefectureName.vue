4f06cdb1da156f67493843e245e085087d1982fb
Ryutaro0802 <ne200152g@gmail.com>
2019-07-23 20:38:03 +0900

    かなに変換

---
+++ pages/prefecture/_prefectureName.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <h1 class="heading">
+      {{ prefectureKanaName }}
+    </h1>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+import { State } from "vuex-class"
+
+@Component({
+  components: {}
+})
+export default class extends Vue {
+  validate({ params }: { params: any }): boolean {
+    return /^\w+$/.test(params.prefectureName)
+  }
+  // created() {
+    // console.log(this.$route.params.prefectureName)
+  // }
+  get prefectureKanaName(): string {
+    switch (this.$route.params.prefectureName) {
+      case "hokkaido":
+        return "北海道"
+        break
+      default:
+        return ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+.heading {
+  color: white;
+}
+</style>
