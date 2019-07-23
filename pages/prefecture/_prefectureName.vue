2ff5c2a3208be9581592b25551359527b6f2cc7d
Ryutaro0802 <ne200152g@gmail.com>
2019-07-23 21:04:36 +0900

    カラムコンポーネントを雑に実装

--- pages/prefecture/_prefectureName.vue
+++ pages/prefecture/_prefectureName.vue
@@ -1,33 +1,52 @@
 <template>
   <section>
-    <h1 class="heading">
-      {{ prefectureKanaName }}
-    </h1>
+    <h2 class="heading">{{ prefectureKanaName }}</h2>
+    <button @click.prevent="photoUpload">Upload</button>
+
+    <Column>
+      <ColumnItem>
+        <img src="https://dummyimage.com/200x200/adadad/fff" alt />
+      </ColumnItem>
+      <ColumnItem>
+        <img src="https://dummyimage.com/200x200/adadad/fff" alt />
+      </ColumnItem>
+    </Column>
   </section>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "nuxt-property-decorator"
-import { State } from "vuex-class"
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
+import Column from "~/components/column/Column.vue";
+import ColumnItem from "~/components/column/ColumnItem.vue";
 
 @Component({
-  components: {}
+  components: {
+    Column,
+    ColumnItem
+  }
 })
 export default class extends Vue {
   validate({ params }: { params: any }): boolean {
-    return /^\w+$/.test(params.prefectureName)
+    return /^\w+$/.test(params.prefectureName);
   }
-  // created() {
-    // console.log(this.$route.params.prefectureName)
-  // }
+
   get prefectureKanaName(): string {
-    switch (this.$route.params.prefectureName) {
+    const prefectureRomaName = this.$route.params.prefectureName;
+    switch (prefectureRomaName) {
       case "hokkaido":
-        return "北海道"
-        break
+        return "北海道";
+        break;
+      case "aomori":
+        return "青森県";
+        break;
       default:
-        return ""
+        return "";
     }
+  }
+
+  photoUpload() {
+    console.log("upload");
   }
 }
 </script>
